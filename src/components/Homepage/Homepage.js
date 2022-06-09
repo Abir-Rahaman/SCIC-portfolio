4a77e13c13153d8d3c4ad6e163f641d77fdad18f
Md. Abir Rahaman <abirahman.cse@gmail.com>
2022-06-09 17:18:14 +0600

    skil section done

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../Shared/Navbar';
 import About from '../About';
 import Banner from '../Banner';
+import Skill from '../Skill';
 
 
 const Homepage = () => {
@@ -10,6 +11,7 @@ const Homepage = () => {
             <Navbar>
                <Banner></Banner>
                <About></About>
+               <Skill></Skill>
             </Navbar>
            
         </div>
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,10 +1,88 @@
 import React from 'react';
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import bs from '../assets/bootstrap.png'
+import js from '../assets/javascript.png'
+import tailwind from '../assets/tailwind.png'
+import react from '../assets/react.png'
+import firebase from '../assets/firebase.png'
+import node from '../assets/node.png'
+import mongo from '../assets/mongo.png'
 
 const Skill = () => {
     return (
-        <div>
-            
-        </div>
+        <div name='skill' className='text-[#95bbd4] lg:px-96 md:px-44' >
+            <div className="text-center">
+                <p className='text-4xl inline border-b-4 border-accent'>My Skills</p>
+                <p className='text-2xl mt-8'> These Are Technologies I'he worked with</p>
+            </div>
+              <div className="grid lg:grid-cols-3 grid-cols-2">
+              <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={html} alt="" />
+                <p className='my-4 text-accent text-center font-bold'>HTML5</p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={css} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> CSS3</p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={bs} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> BOOTSTRAP5 </p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={js} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> JAVASCRIPT </p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={tailwind} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> TAILWIND </p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={react} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> REACT </p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={firebase} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> FIREBASE </p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={node} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> NODE JS </p>
+                
+            </div>
+            </div>
+            <div class="card w-96 bg-base-100  mt-9">
+            <div class="card-body hover:scale-150 duration-500 ">
+                <img className='w-24 mx-auto mt-8' src={mongo} alt="" />
+                <p className='my-4 text-accent text-center font-bold'> MONGO DB </p>
+                
+            </div>
+            </div>
+              </div>
+            </div>
+
     );
 };
 
